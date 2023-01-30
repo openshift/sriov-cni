995e4a6c7e6f12a042d1b839f8a502f911154fa5
Sebastian Sch <sebassch@gmail.com>
2023-01-30 13:31:57 +0200

    Add both hardware and nic mac allocation retry
    This commit add a retry function for all the mac address allocation
    functions.
    this is needed because some of the drivers have async mac allocation
    or get a device or resouce busy errors until the vf device is fully
    configured on the system so instead of failing the CNI we just wait
    100 miliseconds
    
    Signed-off-by: Sebastian Sch <sebassch@gmail.com>

--- pkg/sriov/sriov_suite_test.go
+++ pkg/sriov/sriov_suite_test.go
@@ -1,11 +1,12 @@
 package sriov
 
 import (
-	"github.com/k8snetworkplumbingwg/sriov-cni/pkg/utils"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 
 	"testing"
+
+	"github.com/k8snetworkplumbingwg/sriov-cni/pkg/utils"
 )
 
 func TestConfig(t *testing.T) {
